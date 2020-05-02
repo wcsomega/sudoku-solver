186ca9e89a86a7a6540824a0ecf75e65162f94a5
wcsomega <wcsomega@gmail.com>
2020-05-01 22:40:43 -0400

    Add state to game board component. Add Immutable js library. Also add getter and setter helper functions for a 2d array. Change title in HTML to be more descriptive. Remove unused CSS classes.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,77 @@
 import React from 'react';
+import { List } from 'immutable';
 import './App.css';
 
-function App() {
-  let elements = [];
-  for(let y = 0; y < 9; y++) {
-    for(let x = 0; x < 9; x++){
-      let classes = ["cell"]
-      if (y !== 0) {
-        if (y % 3 === 0){
-          classes.push("border-black-t");
-        } else {
-          classes.push("border-gray-t");
-        }
-      }
-      if (x !== 0) {
-        if (x % 3 === 0) {
-          classes.push("border-black-l");
-        } else {
-          classes.push("border-gray-l");
-        }
-      }
-      if (x === 0 && y === 0) {
-        classes.push("rounded-tl");
-      }
-      if (x === 8 && y === 0) {
-        classes.push("rounded-tr");
-      }
-      if (x === 0 && y === 8) {
-        classes.push("rounded-bl");
-      }
-      if (x === 8 && y === 8) {
-        classes.push("rounded-br");
-      }
-      elements.push(
-        <div className={classes.join(" ")}>
-          <span>{" "}</span>
-        </div>
-      );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    let nums = List(Array(81).fill(""));
+    this.state = {
+      numbers: nums,
     }
   }
-  return (
-    <div className="grid width-540 height-540 mx-auto mt-40 border-black rounded">
-      {elements}
-    </div>
-  );
-}
 
-export default App;
+  getAt = (x, y) => {
+    const i = y * 9 + x;
+    return this.state.numbers.get(i);
+  }
+
+  setAt = (x, y, value) => {
+    const i = y * 9 + x;
+    this.setState({
+      numbers: this.state.numbers.set(i, value),
+    })
+  }
+
+  handleClick = (x, y) => {
+    const num = Math.floor(Math.random() * 9 + 1);
+    this.setAt(x, y, num);
+  }
+
+  render() {
+    let elements = [];
+    for(let y = 0; y < 9; y++) {
+      for(let x = 0; x < 9; x++){
+        let classes = ["cell"]
+        if (y !== 0) {
+          if (y % 3 === 0){
+            classes.push("border-black-t");
+          } else {
+            classes.push("border-gray-t");
+          }
+        }
+        if (x !== 0) {
+          if (x % 3 === 0) {
+            classes.push("border-black-l");
+          } else {
+            classes.push("border-gray-l");
+          }
+        }
+        if (x === 0 && y === 0) {
+          classes.push("rounded-tl");
+        }
+        if (x === 8 && y === 0) {
+          classes.push("rounded-tr");
+        }
+        if (x === 0 && y === 8) {
+          classes.push("rounded-bl");
+        }
+        if (x === 8 && y === 8) {
+          classes.push("rounded-br");
+        }
+        let key= y*9+x;
+        elements.push(
+          <div key={key.toString()} className={classes.join(" ")}
+            onClick={() => this.handleClick(x, y)}>
+            <span>{this.getAt(x, y)}</span>
+          </div>
+        );
+      }
+    }
+    return (
+      <div className="grid width-540 height-540 mx-auto mt-40 border-black rounded">
+        {elements}
+      </div>
+    );
+  }
+}
